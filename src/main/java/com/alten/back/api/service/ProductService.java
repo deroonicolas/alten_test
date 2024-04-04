141c8273473960c021fac196a69e248edbd1b424
Nicolas Deroo <deroo.nicolas@gmail.com>
2024-04-04 17:56:20 +0200

    Product service

---
+++ src/main/java/com/alten/back/api/service/ProductService.java
@@ -0,0 +1,52 @@
+package com.alten.back.api.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alten.back.api.model.Product;
+import com.alten.back.api.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    /**
+     * Gets the product with the specified id
+     * @param id The id of the product
+     * @return The product object if exists
+     */
+    public Optional<Product> getProduct(final Long id) {
+        return productRepository.findById(id);
+    }
+
+    /**
+     * Gets all products
+     * @return All product objects
+     */
+    public Iterable<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    /**
+     * Deletes a product with the specified id
+     * @param id Id of the product to delete
+     */
+    public void deleteProduct(final Long id) {
+    	productRepository.deleteById(id);
+    }
+
+    /**
+     * Saves a product
+     * @param product The product object
+     * @return The saved product
+     */
+    public Product saveProduct(Product product) {
+    	Product savedProduct = productRepository.save(product);
+        return savedProduct;
+    }
+
+}
